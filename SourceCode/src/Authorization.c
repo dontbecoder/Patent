076ebd39c43d2f98d70f1a6a24ab1629702b99a4
dontbecoder <ineedachance1@gmail.com>
2017-06-29 18:51:22 +0800

    update by 20170629

--- SourceCode/src/Authorization.c
+++ SourceCode/src/Authorization.c
@@ -4,22 +4,23 @@
 #define TRUE   1
 #define FALSE  0
 
-bool Want_to_modify = TRUE;			// ?????
-bool Want_to_Second_Trail = TRUE;	// ???
-bool Has_prioriy = TRUE;			// ????
+bool Want_to_modify = TRUE;				// 提主动修改
+bool Want_to_advance_announce = TRUE;	// 提前公布申请
+bool Want_to_Second_Trail = TRUE;		// 提实审
+bool Has_prioriy = TRUE;				// 有优先权
 
-#define FAILED_WITHDRAW			0	// ??
-#define FAILED_TERMIATION		0	// ??
-
+#define FAILED_WITHDRAW			0	// 撤回
+#define FAILED_TERMIATION		0	// 终止
+#define FAILED_REJECT			0	// 驳回
 
 // All date 
-#define Priority_date			// ????
+#define Priority_date			// 优先权日
 
-#define Application_date		// ????? 	A28  A42
+#define Application_date		// 实际申请日 	A28  A42
 
-#define RECEIVED_A_NOTICE		// ???????
+#define RECEIVED_A_NOTICE		// 收到通知书之日
 
-#define DATE2APP_SECOND_TRAIL 	// ??????
+#define DATE2APP_SECOND_TRAIL 	// 提出实审之日
 
 #define YEARS_3
 #define YEARS_10
@@ -29,34 +30,35 @@ bool Has_prioriy = TRUE;			// ????
 #define MONTH_3
 #define MONTH_6
 #define MONTH_12
+#define MONTH_18
 
 #define DAYS_15
 
 
-#define Novelty_Grace_period				MONTH_6			// ??????????
-#define Priority_Grace_period_Design		MONTH_6			// ????????? 6???
-#define Priority_Grace_period_Inovention	MONTH_12		// ????????? 12???
-#define Priority_Grace_period_Unity			MONTH_12		// ??????? 12???
+#define Novelty_Grace_period				MONTH_6			// 不丧失新颖性的宽限期
+#define Priority_Grace_period_Design		MONTH_6			// 外观设计优先权期限 6个月内
+#define Priority_Grace_period_Inovention	MONTH_12		// 实用新型优先权期限 12个月内
+#define Priority_Grace_period_Unity			MONTH_12		// 发明优先权期限 12个月内
 
 
 
 // Fees
-// ???????
-#define FEES_APP			// ???
-#define FEES_SECOND_TRAIL	// ???
-#define FEES_REVIEW		// ???
-#define FEES_ANNUAL		// ??  (???????)
+// 以下四种可减缓
+#define FEES_APP			// 申请费
+#define FEES_SECOND_TRAIL	// 实审费
+#define FEES_REVIEW		// 复审费
+#define FEES_ANNUAL		// 年费  (授权起三年年费)
 
 
-#define FEES_APP_PLUS		// ????? (??????10?,?????30?)
-#define FEES_ANNOUNCE_PRINT		// ?????
-#define FEES_WANT_PRIORIY  // ??????
-#define FEES_RECOVER_RIGHT	// ???????
-#define FEES_EXTEND_TIME	// ???????
-#define FEES_REGISTER		// ?????
-#define FEES_WANT_INVALID	// ???????
-#define FEES_PATENT_EVALUATION_REPORT	// ??????????
-#define FEES_RECORD_ITEM_CHANGE			// ???????
+#define FEES_APP_PLUS		// 申请附加费 (权利要求超过10项，说明书超过30页)
+#define FEES_ANNOUNCE_PRINT		// 公布印刷费
+#define FEES_WANT_PRIORIY  // 优先权请求费
+#define FEES_RECOVER_RIGHT	// 恢复权利请求费
+#define FEES_EXTEND_TIME	// 延长期限请求费
+#define FEES_REGISTER		// 专利登记费
+#define FEES_WANT_INVALID	// 无效宣告请求费
+#define FEES_PATENT_EVALUATION_REPORT	// 专利权评价报告请求费
+#define FEES_RECORD_ITEM_CHANGE			// 著录事项变更费
 
 // Patent
 
@@ -154,10 +156,10 @@ int CheckLegal()
 int CheckObviousDefect()
 {}
 	
-int Fisrt_trail() // ?? A3 A40
+int Fisrt_trail() // 初审 A3 A40
 {
 
-	if(Want_to_modify) 	// R51	??????
+	if(Want_to_modify) 	// R51	主动修改时机
 		{
 			switch(Obj_app)
 				{
@@ -177,22 +179,22 @@ int Fisrt_trail() // ?? A3 A40
 		}
 
 
-	if(FALSE==CheckFees())  // ??
+	if(FALSE==CheckFees())  // 费用
 	{
 		return FALSE;
 	}
 
-	if(FALSE==CheckForm())	// ?????????
+	if(FALSE==CheckForm())	// 申请文件的形式审查
 	{
 		return FALSE;
 	}
 
-	if(FALSE==CheckLegal())  // ?????????
+	if(FALSE==CheckLegal())  // 申请文件的法律手续
 	{
 		return FALSE;
 	}
 
-	if(FALSE==CheckObviousDefect())		// ??????? A5  A25
+	if(FALSE==CheckObviousDefect())		// 明显实质性缺陷 A5  A25
 	{
 		return FALSE;
 	}
@@ -200,14 +202,14 @@ int Fisrt_trail() // ?? A3 A40
 	return TRUE;
 }
 
-int Second_Trail() // ??
+int Second_Trail() // 实审
 {
 	if(Want_to_Second_Trail)
 	{
 		if(Priority_date + YEARS_3)  //A35
 		{
 		
-			if(Check3Attrib())	// ???
+			if(Check3Attrib())	// 三性等
 					{
 						return TRUE;
 					}
@@ -222,27 +224,27 @@ int Second_Trail() // ??
 	return FALSE;
 }
 
-int Authorization() // ????
+int Authorization() // 授权程序
 {
 
 }
 
-int Announced()		// ????  A34 
+int Announced()		// 国家公布  A34 
 {
 
 
 }
 
 
-int PatentApplication(PATENT_APPLICATION_ENUM Obj_app)			// ????
+int PatentApplication(PATENT_APPLICATION_ENUM Obj_app)			// 申请流程
 {
 
-	if(FALSE == CheckPepole())   // ????
+	if(FALSE == CheckPepole())   // 申请主体
 	{
 		return FALSE;
 	}
 
-	if(FALSE == CheckObj())		// ????  A25
+	if(FALSE == CheckObj())		// 保护客体  A25
 	{
 		return FALSE;
 	}
@@ -255,7 +257,7 @@ int PatentApplication(PATENT_APPLICATION_ENUM Obj_app)			// ????
 			Authorization()		
 		}
 		else
-		{						// turn down				
+		{						// reject				
 			return FALSE;
 		}
 			
@@ -276,7 +278,8 @@ int PatentApplication(PATENT_APPLICATION_ENUM Obj_app)			// ????
 	case INOVENTION:
 		if(Fisrt_trail())		
 			{
-			 	Announced()		// ????
+				if((Priority_date + MONTH_18)||(Want_to_advance_announce))
+			 		Announced()												// 国家公布
 			 
 				if(Second_Trail())
 					{
@@ -302,7 +305,7 @@ int PatentApplication(PATENT_APPLICATION_ENUM Obj_app)			// ????
 }
 
 
-int ProtectPatent()   // ????
+int ProtectPatent()   // 保护程序
 {
 
 
