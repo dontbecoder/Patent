2a07e85e83c33469a6ec58d0c2142b6f61ccd462
dontbecoder <ineedachance1@gmail.com>
2017-07-01 14:21:24 +0800

    update by 20170701

--- SourceCode/src/Authorization.c
+++ SourceCode/src/Authorization.c
@@ -7,11 +7,20 @@
 bool Want_to_modify = TRUE;				// 提主动修改
 bool Want_to_advance_announce = TRUE;	// 提前公布申请
 bool Want_to_Second_Trail = TRUE;		// 提实审
-bool Has_prioriy = TRUE;				// 有优先权
+bool Want_to_Review_BasedOn_ChangedAtInternationalStage = TRUE;		// 以国际阶段做的修改作为审查基础的
+bool Want_Priority = TRUE;				// 要求优先权
+bool Has_priority = TRUE;				// 有优先权
 
-#define FAILED_WITHDRAW			0	// 撤回
-#define FAILED_TERMIATION		0	// 终止
-#define FAILED_REJECT			0	// 驳回
+#define FAILED_WITHDRAW				0	// 撤回
+#define FAILED_TERMIATION			0	// 终止
+#define FAILED_REJECT				0	// 驳回
+#define FAILED_EFFECT_TERMINAT		0	// 效力终止
+#define FAILED_NOT_REQUIRED			0	// 未要求
+
+#define FAILED_WITHDRAW_FOR_PCT			0	// PCT撤回
+#define FAILED_WITHDRAWED_FOR_PCT		0	// PCT被视为撤回
+#define FAILED_WITHDRAW_CHINA_FOR_PCT	0	// PCT指定中国被撤回的
+
 
 // All date 
 #define Priority_date			// 优先权日
@@ -22,6 +31,8 @@ bool Has_prioriy = TRUE;				// 有优先权
 
 #define DATE2APP_SECOND_TRAIL 	// 提出实审之日
 
+#define PCT_EnterNational_date	// PCT进入国家阶段之日(即进入日)
+
 #define YEARS_3
 #define YEARS_10
 #define YEARS_20
@@ -31,6 +42,7 @@ bool Has_prioriy = TRUE;				// 有优先权
 #define MONTH_6
 #define MONTH_12
 #define MONTH_18
+#define MONTH_30
 
 #define DAYS_15
 
@@ -52,7 +64,7 @@ bool Has_prioriy = TRUE;				// 有优先权
 
 #define FEES_APP_PLUS		// 申请附加费 (权利要求超过10项，说明书超过30页)
 #define FEES_ANNOUNCE_PRINT		// 公布印刷费
-#define FEES_WANT_PRIORIY  // 优先权请求费
+#define FEES_WANT_PRIORITY  // 优先权请求费
 #define FEES_RECOVER_RIGHT	// 恢复权利请求费
 #define FEES_EXTEND_TIME	// 延长期限请求费
 #define FEES_REGISTER		// 专利登记费
@@ -86,6 +98,12 @@ PATENT_APPLICATION_ENUM Obj_app = 0;
 
 
 /***********************/
+int Recover_right()  // 恢复权利流程
+{
+
+}
+
+
 int CheckPepole()
 {
 
@@ -137,13 +155,13 @@ int CheckFees()
 	)														// R95
 		{
 		
-		FEES_APP + (FEES_APP_PLUS) + (FEES_WANT_PRIORIY) + FEES_ANNOUNCE_PRINT
+		FEES_APP + (FEES_APP_PLUS) + (FEES_WANT_PRIORITY) + FEES_ANNOUNCE_PRINT
 
 		}
 
-	if(Has_prioriy)
+	if(Has_priority)
 	{
-		FEES_WANT_PRIORIY
+		FEES_WANT_PRIORITY
 	}
 }
 
@@ -304,6 +322,107 @@ int PatentApplication(PATENT_APPLICATION_ENUM Obj_app)			// 申请流程
 	return TRUE;
 }
 
+
+/************************		PCT 申请						**********************************/
+
+int Check_written_statement()
+{
+	// 受理国际局 、国际申请号 、申请的专利类型
+	// (遗传资源披露表)	R109 (if necessary)
+	
+	// written in Chinese
+}
+int CheckFees_For_PCT() // R93 && R103
+{
+	if(Want_Priority)
+	{	
+		if(PCT_EnterNational_date + MONTH_2)	// R110
+		{
+		FEES_WANT_PRIORITY
+			}
+		else
+			return FAILED_NOT_REQUIRED;
+	}
+	
+	
+}
+int CheckFiles_in_Chinese()
+{
+	// 说明书、权利要求书的中文译文
+}
+int Give_prove_for_EnterNationalStage() // R104
+{
+	int date;
+	
+	date = PCT_EnterNational_date;	// 给予国家进入日 、 国家申请号
+}
+
+int CheckOther4Condition_For_PCT() // R104 PCT 进入国家阶段时，另4项手续要求
+{
+}
+int CorrectFlow_For_PCT()
+{
+
+}
+
+int Enter_National_Stage() // 进入国家阶段
+{
+	if(FALSE == Check_written_statement())
+		return FAILED_EFFECT_TERMINAT;
+
+	if(FALSE == CheckFees_For_PCT())
+		return FAILED_EFFECT_TERMINAT;
+
+	if(FALSE == CheckFiles_in_Chinese())
+		return FAILED_EFFECT_TERMINAT;
+
+	Give_prove_for_EnterNationalStage();	
+
+	if(TRUE == CheckOther4Condition_For_PCT())
+		return TRUE;
+	else
+		{
+		if(TRUE == CorrectFlow_For_PCT())
+		  return TRUE;
+		else
+		{
+			if(Recover_right())  // FAILED_WITHDRAW
+				return FAILED_EFFECT_TERMINAT;
+			else
+				return TRUE;			
+		}
+	}
+
+	if(Want_to_Review_BasedOn_ChangedAtInternationalStage)		// R106
+		{	(PCT_EnterNational_date + MONTH_2)	}
+
+
+}
+
+
+int PatentApplication_For_PCT(PATENT_APPLICATION_ENUM Obj_app)
+{
+	if(FAILED_WITHDRAW_FOR_PCT || FAILED_WITHDRAWED_FOR_PCT || FAILED_WITHDRAW_CHINA_FOR_PCT)  // 国际阶段   R105
+		return FAILED_EFFECT_TERMINAT;
+	
+	if(Priority_date + MONTH_30)
+	{
+		Enter_National_Stage();
+	}
+	else if (Priority_date + MONTH_30 + MONTH_2)
+	{
+	  
+		if(Recover_right())
+			Enter_National_Stage();
+
+	}else{
+		
+		return FAILED_EFFECT_TERMINAT;  // R105
+	}
+}
+
+
+/***********************		专利权的保护								********************************************/
 
 int ProtectPatent()   // 保护程序
 {
